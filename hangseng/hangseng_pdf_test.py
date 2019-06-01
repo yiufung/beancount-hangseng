d3e7df5788407afff3ef741feeb904268ce016d4
Cheong Yiu Fung <cheongyiufung@gmail.com>
2019-06-02 00:57:10 +0800

    init commit

---
+++ hangseng/hangseng_pdf_test.py
@@ -0,0 +1,19 @@
+"""Unit tests for Hang Seng PDF importer (using pytest)."""
+__copyright__ = "Copyright (C) 2019 Cheong Yiu Fung"
+__license__ = "GNU GPLv2"
+
+from os import path
+import pytest
+
+from beancount.ingest import regression_pytest as regtest
+from . import hangseng_pdf
+
+
+IMPORTER = hangseng_pdf.Importer("Assets:HK:HangSeng")
+
+@pytest.mark.skipif(not hangseng_pdf.is_pdftotext_installed(),
+                    reason="pdftotext is not installed")
+@regtest.with_importer(IMPORTER)
+@regtest.with_testdir(path.dirname(__file__))
+class TestImporter(regtest.ImporterTestBase):
+    pass
