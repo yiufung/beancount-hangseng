3e1c6759a64ddae9dc5110abfa2cba04f693d996
Cheong Yiu Fung <cheongyiufung@gmail.com>
2019-06-03 00:39:15 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="beancount-hangseng",
+    version="0.0.4",
+    author="Cheong Yiu Fung",
+    author_email="mail@yiufung.net",
+    description="Imports Hang Seng Integrated Account eStatements to beancount",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yiufung/beancount-hangseng",
+    install_requires=['beancount'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Office/Business :: Financial :: Accounting",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
