433803d4b719dfefd9d9938e29df5315fbfdc87a
Cheong Yiu Fung <cheongyiufung@gmail.com>
2019-06-03 16:38:06 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 setuptools.setup(
     name="beancount-hangseng",
-    version="0.1.0",
+    version="0.1.1",
     author="Cheong Yiu Fung",
     author_email="mail@yiufung.net",
     description="Parse Hang Seng eStatements to Beancount/CSV",
